59144948c685ca051933d9982f1bcb5227027dd6
afzal63 <42829235+afzal63@users.noreply.github.com>
2018-12-20 19:37:05 +0530

    Code Uploaded

---
+++ Section 2/demo.js
@@ -0,0 +1,11 @@
+var i;
+
+console.log("Start!"); \\Printing the word start
+
+for(i=10; i<=20; i++){  \\Setting i as 10 and incrementing it till 20
+	console.log("i is " +i);
+}
+
+console.log("Now i is:" +i); \\Printing the final value before we end
+
+console.log("End!");
